dfd7d74b5015cc7a22d83366a122814bd2a355cd
Casper Van Gheluwe <casper.vangheluwe@ugent.be>
2016-08-03 14:57:10 +0200

    Debounce changes to avoid excessive redrawing

--- addon/components/mu-heatmap.js
+++ addon/components/mu-heatmap.js
@@ -7,6 +7,8 @@ export default Ember.Component.extend({
   dataset: null,
   latitude: 0.0,
   longitude: 0.0,
+  bounds: undefined,
+  center: undefined,
   zoom: 7,
 
   backgroundColor: null,
@@ -41,16 +43,27 @@ export default Ember.Component.extend({
           }
         ));
 
-        self.set('data', Ember.Object.create({data: data}));
+        var min = Number.MAX_SAFE_INTEGER;
+        var max = Number.MIN_SAFE_INTEGER;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].value < min) {
+            min = data[i].value;
+          }
+          if (data[i].value > max) {
+            max = data[i].value;
+          }
+        }
+
+        self.set('data', Ember.Object.create({min: min, max: max, data: data}));
       });
   }).on('init'),
 
   heatmapRefresher: Ember.observer('latitude', 'longitude', 'zoom', 'backgroundColor', 'blur', 'gradient', 'maxOpacity',
-    'minOpacity', 'radius', 'scaleRadius', 'useLocalExtrema', function () {
+    'opacity', 'minOpacity', 'radius', 'scaleRadius', 'useLocalExtrema', function () {
       var self = this;
       self.set('waitingUpdates', self.get('waitingUpdates')+1);
-      Ember.run.next(function () {
+      Ember.run.debounce({'self': this}, function () {
         self.set('waitingUpdates', self.get('waitingUpdates')-1);
-      });
+      }, 100);
     }).on('init')
 });
